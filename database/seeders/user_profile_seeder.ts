d3e78f1605440367b9d8fe4e719674620c4bc5a9
Dmokrite1 <hg_greg@proton.me>
2024-03-11 16:49:15 +0100

    Added seeder announce_seeder, practice_seeder, user_profile_seeder, user_seeder

---
+++ database/seeders/user_profile_seeder.ts
@@ -0,0 +1,59 @@
+import UserProfile from '#models/user_profile'
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+
+export default class extends BaseSeeder {
+  async run() {
+    await UserProfile.createMany([
+      {
+        id: 1,
+        userId: 1,
+        genre: 'bizarre',
+        orientation: 'à gauche?',
+        size: 5,
+        weight: 12,
+        hairColor: 'auburn',
+        eyeColor: 'jaune cyrose'
+      },
+      {
+        id: 2,
+        userId: 2,
+        genre: 'bizarre',
+        orientation: 'à droite?',
+        size: 5,
+        weight: 12,
+        hairColor: 'auburn',
+        eyeColor: 'jaune cyrose'
+      },
+      {
+        id: 3,
+        userId: 3,
+        genre: 'bizarre',
+        orientation: 'en haut?',
+        size: 5,
+        weight: 12,
+        hairColor: 'auburn',
+        eyeColor: 'jaune cyrose'
+      },
+      {
+        id: 4,
+        userId: 4,
+        genre: 'bizarre',
+        orientation: 'en bas?',
+        size: 5,
+        weight: 12,
+        hairColor: 'auburn',
+        eyeColor: 'jaune cyrose'
+      },
+      {
+        id: 5,
+        userId: 5,
+        genre: 'bizarre',
+        orientation: 'tout droit?',
+        size: 5,
+        weight: 12,
+        hairColor: 'auburn',
+        eyeColor: 'jaune cyrose'
+      }
+    ])
+  }
+}
---
+++ database/seeders/user_seeder.ts
@@ -0,0 +1,79 @@
+import User from '#models/user'
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+
+export default class extends BaseSeeder {
+  async run() {
+    await User.createMany([
+      {
+        id: 1, 
+        firstname: 'Franck', 
+        lastname: 'Tchibozo', 
+        nickname: 'FT', 
+        email: 'ft21@test.com', 
+        password: '1234', 
+        address: {
+          street: 'oulalalala',
+          number: '7',
+          city: 'Berninois',
+          zip_code: 7500       
+        }
+      },
+      {
+        id: 2, 
+        firstname: 'Francky', 
+        lastname: 'Tchibozomique', 
+        nickname: 'FT1', 
+        email: 'ft210@test.com', 
+        password: '1234', 
+        address: {
+          street: 'oulalalala2',
+          number: '7b',
+          city: 'Berninois',
+          zip_code: 7500       
+        }
+      },
+      {
+        id: 3, 
+        firstname: 'Franckyz', 
+        lastname: 'Thé', 
+        nickname: 'FT2', 
+        email: 'ft214@test.com', 
+        password: '1234', 
+        address: {
+          street: 'oulalalala3',
+          number: '7c',
+          city: 'Berninois',
+          zip_code: 7500       
+        }
+      },
+      {
+        id: 4, 
+        firstname: 'Franckyz', 
+        lastname: 'Thés', 
+        nickname: 'FT3', 
+        email: 'ft2140@test.com', 
+        password: '1234', 
+        address: {
+          street: 'oulalalala31',
+          number: '7c',
+          city: 'Berninois',
+          zip_code: 7500       
+        }
+      },
+      {
+        id: 5, 
+        firstname: 'Francis', 
+        lastname: 'Tchizobi', 
+        nickname: 'FT24', 
+        email: 'ft2148@test.com', 
+        password: '1234', 
+        address: {
+          street: 'oulalalala7',
+          number: '7c',
+          city: 'Berninois',
+          zip_code: 7500       
+        }
+      }
+    ])
+  }
+}
