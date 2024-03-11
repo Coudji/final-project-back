294282d5c7c18aa1f5be56a7c703ce898d99224b
Coudji <coudji@ymail.com>
2024-03-11 10:49:07 +0100

    init

---
+++ start/routes.ts
@@ -0,0 +1,36 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
+const NeuneusController = () => import ('#controllers/neuneus_controller')
+const AuthController = () => import('#controllers/auth_controller')
+
+router.get('/', async () => {
+  return {
+    hello: 'world',
+  }
+})
+
+
+
+router.group(() => {
+
+  router.group(()=>{
+    
+    router.post('register', [AuthController, 'register'])
+    router.post('login', [AuthController, 'login'])
+    router.get('me', async({auth,response}) => {
+      const user = await auth.authenticate()
+      return response.ok(user)
+    }).use(middleware.auth())
+  }).prefix('user')
+  
+  router.resource('neuneu', NeuneusController).use(['index','update'],middleware.auth())
+}).prefix('api')
