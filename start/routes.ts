9c00effecb42ce7a19aef0d89ae4b960819b2193
Dmokrite1 <hg_greg@proton.me>
2024-03-15 10:21:40 +0100

    add routes_announce

--- start/routes.ts
+++ start/routes.ts
@@ -9,6 +9,7 @@
 
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
+import AnnounceController from '#controllers/announce_controller'
 const AuthController = () => import('#controllers/auth_controller')
 const UserController = () => import('#controllers/user_controller')
 const PracticesController = () => import ('#controllers/practices_controller')
@@ -51,6 +52,17 @@ router
         router.delete(':id', [UserController, 'destroy'])
       })
       .prefix('user')
+      .use(middleware.auth())
+
+    router
+      .group(() => {
+        router.get('', [AnnounceController, 'getAllAnnounces'])
+        router.get(':id', [AnnounceController, 'getOneAnnounce'])
+        router.post('', [AnnounceController, 'createAnnounce'])
+        router.patch(':id', [AnnounceController, 'updateAnnounce'])
+        router.delete(':id', [AnnounceController, 'deleteAnnounce'])
+      })
+      .prefix('announce')
       .use(middleware.auth())
 
     router.get('practices', [PracticesController,'getAllPractices']).use(middleware.auth())
