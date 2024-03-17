9d768989e2496c2d1ce1214c4a66c69f7f2b4ccd
Coudji <106971118+Coudji@users.noreply.github.com>
2024-03-17 22:11:47 +0100

    fix TYPE suppression on table drop for truth now ^^'

--- database/migrations/1710107839022_create_user_profiles_table.ts
+++ database/migrations/1710107839022_create_user_profiles_table.ts
@@ -4,16 +4,16 @@ export default class extends BaseSchema {
   protected tableName = 'user_profiles'
 
   async up() {
+    this.schema.raw('DROP TYPE IF EXISTS "user_profile_orientation" CASCADE')
+    this.schema.raw('DROP TYPE IF EXISTS "user_profile_genre" CASCADE')
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.integer('user_id').unsigned().references('users.id').onDelete('CASCADE')
-      //table.string('genre').notNullable()
       table.enum('genre', ['homme', 'femme', 'robot', 'transgenre', 'transexuel', 'transformers'], {
         useNative: true,
         enumName: 'user_profile_genre',
       })
       .notNullable()
-      //table.string('orientation').notNullable()
       table
         .enum('orientation', ['hétéro', 'homo', 'pas difficile', 'plug & play'], {
           useNative: true,
@@ -33,8 +33,8 @@ export default class extends BaseSchema {
   }
 
   async down() {
-    this.schema.raw('DROP TYPE IF EXIST "user_profile_orientation" CASCADE')
-    this.schema.raw('DROP TYPE IF EXIST "user_profile_genre" CASCADE')
+    this.schema.raw('DROP TYPE IF EXISTS "user_profile_orientation" CASCADE')
+    this.schema.raw('DROP TYPE IF EXISTS "user_profile_genre" CASCADE')
     this.schema.dropTable(this.tableName)
   }
 }
