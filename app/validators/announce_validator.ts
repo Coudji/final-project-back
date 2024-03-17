4a3ea80936246e60fa5a76b6d829f789ee28ff18
Dmokrite <hg_greg@msn.com>
2024-03-17 16:40:06 +0100

    check and add restrictions on the migrations and validators folder + add announce_validator in the announce_controller

--- app/validators/announce_validator.ts
+++ app/validators/announce_validator.ts
@@ -3,8 +3,8 @@ import vine from '@vinejs/vine';
 export const announceValidator = vine.compile(
   vine.object({
     userId: vine.number(),
-    title: vine.string(),
-    description: vine.string().minLength(3).maxLength(20),
+    title: vine.string().maxLength(64),
+    description: vine.string().minLength(3).maxLength(600),
     infidelityCard: vine.boolean(),
     private: vine.boolean(),
     escort: vine.boolean(),
