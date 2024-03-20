a7c12815c797d87acae57e2a90cf58114802fe48
Coudji <106971118+Coudji@users.noreply.github.com>
2024-03-20 23:25:55 +0100

    add genre counter

--- app/controllers/admin_controller.ts
+++ app/controllers/admin_controller.ts
@@ -8,11 +8,19 @@ export default class AdminController {
         const adminCount = await db.rawQuery('select count(*) as total_admins from users where user_type = ?',['admin'])
         const manCount = await db.query().from('user_profiles').count('*').where('genre','homme')
         const womenCount = await db.query().from('user_profiles').count('*').where('genre','femme')
+        const transgenreCount = await db.query().from('user_profiles').count('*').where('genre','transgenre')
+        const transexuelCount = await db.query().from('user_profiles').count('*').where('genre','transexuel')
+        const robotCount = await db.query().from('user_profiles').count('*').where('genre','robot')
+        const transformersCount = await db.query().from('user_profiles').count('*').where('genre','transformers')
         return response.ok({
             userCount: userCount.rows[0].total_users,
             adminCount: adminCount.rows[0].total_admins,
             manCount: manCount[0].count,
             womenCount: womenCount[0].count,
+            transgenreCount:transgenreCount[0].count,
+            transexuelCount:transexuelCount[0].count,
+            robotCount:robotCount[0].count,
+            transformersCount:transformersCount[0].count,
         })
     }
 }
