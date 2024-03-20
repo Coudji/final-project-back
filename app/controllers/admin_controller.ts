029144604ca44f3526d148b4a23b51b88c320eee
Coudji <106971118+Coudji@users.noreply.github.com>
2024-03-20 17:36:20 +0100

    add genre counter

--- app/controllers/admin_controller.ts
+++ app/controllers/admin_controller.ts
@@ -6,9 +6,13 @@ export default class AdminController {
     async statsCounter({ response }:HttpContext) {
         const userCount = await db.rawQuery('select count(*) as total_users from users')
         const adminCount = await db.rawQuery('select count(*) as total_admins from users where user_type = ?',['admin'])
+        const manCount = await db.query().from('user_profiles').count('*').where('genre','homme')
+        const womenCount = await db.query().from('user_profiles').count('*').where('genre','femme')
         return response.ok({
+            userCount: userCount.rows[0].total_users,
             adminCount: adminCount.rows[0].total_admins,
-            userCount: userCount.rows[0].total_users
+            manCount: manCount[0].count,
+            womenCount: womenCount[0].count,
         })
     }
 }
